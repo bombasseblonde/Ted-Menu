25854c740f2444751aa337a2d3fd8fc06cc2da0c
bombasseblonde <misterjonathansands@gmail.com>
2017-02-16 22:45:24 +0100

    Menu de base

---
+++ Ted/Assets/Scripts/ChangeMusic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMusic : MonoBehaviour
+{
+    public AudioClip SinglePlayerMusic;
+
+
+    private AudioSource source;
+
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+
+    void OnLevelWasLoaded(int level)
+    {
+        if (level == 1)
+        {
+            source.clip = SinglePlayerMusic;
+            source.Play();
+        }
+
+    }
+}
